75ca236ea987b797cd1b826f6f85f0f8eda8334a
Arthur Avner <arthuravner@hotmail.com>
2020-03-12 18:07:17 -0300

    Update anilha.py

--- anilha.py
+++ anilha.py
@@ -3,7 +3,8 @@ from OpenGL.GLU import *
 from OpenGL.GL import *
 import math
 
-vertices = []
+verticesBaseBaixo = []
+verticesBaseCima = []
 
 n = 20 #número de lados
 r = 2 #raio
@@ -16,22 +17,49 @@ for i in range(0,n):
     x1 = r/2*math.cos(i*a)
     z1 = r/2*math.sin(i*a)
    
-    vertices += [[x,0,z]]
-    vertices += [[x1,0,z1]]           
+    verticesBaseBaixo += [[x,0,z]]
+    verticesBaseBaixo += [[x1,0,z1]]
+
+    verticesBaseCima += [[x,1,z]]
+    verticesBaseCima += [[x1,1,z1]]           
         
 cores = ( (1,0,0),(1,1,0),(0,1,0),(0,1,1),(0,0,1),(1,0,1),(0.5,1,1),(1,0,0.5) )   
 
 def Anilha():
+    #Desenha a base de baixo da anilha
     glBegin(GL_TRIANGLE_STRIP)
     #glColor3f(1,1,0)
     i = 0
-    for vertex in vertices:
+    for vertex in verticesBaseBaixo:
         glColor3fv(cores[i%len(cores)])        
         glVertex3fv(vertex)
         i = i + 1
-    glVertex3fv(vertices[0])
-    glVertex3fv(vertices[1])
+    glVertex3fv(verticesBaseBaixo[0])
+    glVertex3fv(verticesBaseBaixo[1])
     glEnd()
+
+    #Desenha a base de cima da anilha
+    glBegin(GL_TRIANGLE_STRIP)
+    #glColor3f(1,1,0)
+    i = 0
+    for vertex in verticesBaseCima:
+        glColor3fv(cores[i%len(cores)])        
+        glVertex3fv(vertex)
+        i = i + 1
+    glVertex3fv(verticesBaseCima[0])
+    glVertex3fv(verticesBaseCima[1])
+    glEnd()
+
+    #Desenha lados da anilha
+    glBegin(GL_QUAD_STRIP)
+    glColor3f(1,0,0)    
+    glVertex3fv(verticesBaseBaixo[n-1])
+    glVertex3fv(verticesBaseCima[n-1])
+    for i in range(0,n):
+        #glColor3fv(cores[i%len(cores)])        
+        glVertex3fv(verticesBaseBaixo[i])
+        glVertex3fv(verticesBaseCima[i])
+    glEnd()      
 
 def desenha():
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
@@ -47,7 +75,7 @@ def timer(i):
 glutInit(sys.argv)
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
 glutInitWindowSize(800,600)
-glutCreateWindow("PRISMA")
+glutCreateWindow("ANILHA")
 glutDisplayFunc(desenha)
 glEnable(GL_MULTISAMPLE)
 glEnable(GL_DEPTH_TEST)
